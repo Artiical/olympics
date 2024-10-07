15a5c2ff30a983bc8720c33da5ecf02340842d99
Artical <nassim.rahamnia@gmail.com>
2024-10-07 17:33:47 +0200

    couvrance des tests à 100%  et correction du bug de __main.py

--- olympics/__main__.py
+++ olympics/__main__.py
@@ -5,33 +5,34 @@ import argparse
 from . import cli
 
 parser = argparse.ArgumentParser(
-    prog='olympics',
-    description='Display various information about Olympics results',
+    prog="olympics",
+    description="Display various information about Olympics results",
 )
 parser.add_argument(
-    'command',
-    help='command to launch',
-    choices=('countries', 'collective', 'individual'),
+    "command",
+    help="command to launch",
+    choices=("countries", "collective", "individual"),
 )
 parser.add_argument(
-    '--top',
-    help='number of top elements to display',
-    type=float,
+    "--top",
+    help="number of top elements to display",
+    type=int,  # correction du bug
     default=10,
 )
+
 
 def main(argv=None):
     args = parser.parse_args(argv)
     if (top := int(args.top)) <= 0:
-        raise argparse.ArgumentTypeError(f'{top} is not a positive number')
+        raise argparse.ArgumentTypeError(f"{top} is not a positive number")
     match args.command:
-        case 'countries':
+        case "countries":
             cli.top_countries(top)
-        case 'collective':
+        case "collective":
             cli.top_collective(top)
-        case 'individual':
+        case "individual":
             cli.top_individual(top)
 
 
-if __name__ == '__main__':  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,54 @@
 from olympics.__main__ import main
+import argparse
+import pytest
 
 
 def test_countries():
-    argv = ['countries']
+    argv = ["countries"]
     main(argv)
+
+
+def test_collective():
+    argv = ["collective"]
+    main(argv)
+
+
+def test_individual():
+    argv = ["individual"]
+    main(argv)
+
+
+def test_error():
+    with pytest.raises(argparse.ArgumentTypeError):
+        argv = ["individual", "--top", "-10"]
+        main(argv)
+
+
+def test_countries_with_top():
+    argv = ["countries", "--top", "5"]
+    main(argv)
+
+
+def test_collective_with_top():
+    argv = ["collective", "--top", "3"]
+    main(argv)
+
+
+def test_individual_with_top():
+    argv = ["individual", "--top", "7"]
+    main(argv)
+
+
+def test_invalid_command():
+    with pytest.raises(SystemExit):
+        argv = ["invalid_command"]
+        main(argv)
+
+
+def test_missing_argument():
+    with pytest.raises(SystemExit):
+        argv = []
+        main(argv)
+
+
+# j'ai essayé d'aller plus loin pour les tests
