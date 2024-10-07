7c7f57df5e34b895058be620cc3f4789fa3ace0e
Artical <nassim.rahamnia@gmail.com>
2024-10-07 16:00:50 +0200

    résolution du bug de cli.py

--- olympics/cli.py
+++ olympics/cli.py
@@ -9,19 +9,19 @@ from . import db
 def top_countries(top=10, file=None):
     table = Table(title=f"Top {top} countries")
 
-    table.add_column('Country')
-    table.add_column('Gold', justify='right')
-    table.add_column('Silver', justify='right')
-    table.add_column('Bronze', justify='right')
-    table.add_column('Total', justify='right')
+    table.add_column("Country")
+    table.add_column("Gold", justify="right")
+    table.add_column("Silver", justify="right")
+    table.add_column("Bronze", justify="right")
+    table.add_column("Total", justify="right")
 
     for row in db.get_top_countries(top):
         table.add_row(
-            row['name'],
-            str(row['gold']),
-            str(row['silver']),
-            str(row['bronze']),
-            str(row['gold'] + row['silver'] + row['gold']),
+            row["name"],
+            str(row["gold"]),
+            str(row["silver"]),
+            str(row["bronze"]),
+            str(row["gold"] + row["silver"] + row["bronze"]),  # correction du bug
         )
 
     console = Console(file=file)
@@ -29,15 +29,15 @@ def top_countries(top=10, file=None):
 
 
 def top_collective(top=10, file=None):
-    table = Table(title=f'Top {top} collective events')
+    table = Table(title=f"Top {top} collective events")
 
-    table.add_column('Country')
-    table.add_column('Medals', justify='right')
+    table.add_column("Country")
+    table.add_column("Medals", justify="right")
 
     for row in db.get_top_collective(top):
         table.add_row(
-            row['country'],
-            str(row['medals']),
+            row["country"],
+            str(row["medals"]),
         )
 
     console = Console(file=file)
@@ -45,19 +45,19 @@ def top_collective(top=10, file=None):
 
 
 def top_individual(top=10, file=None):
-    table = Table(title=f'Top {top} individual events')
+    table = Table(title=f"Top {top} individual events")
 
-    table.add_column('Name')
-    table.add_column('Gender')
-    table.add_column('Country')
-    table.add_column('Medals', justify='right')
+    table.add_column("Name")
+    table.add_column("Gender")
+    table.add_column("Country")
+    table.add_column("Medals", justify="right")
 
     for row in db.get_top_individual(top):
         table.add_row(
-            row['name'],
-            row['gender'].capitalize(),
-            row['country'],
-            str(row['medals']),
+            row["name"],
+            row["gender"].capitalize(),
+            row["country"],
+            str(row["medals"]),
         )
 
     console = Console(file=file)
